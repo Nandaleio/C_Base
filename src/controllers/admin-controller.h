90b24959ecc084e70faf565cf285e5caa1da5802
Nandaleio <stephgiorgetti@gmail.com>
2024-12-18 17:58:10 +0100

    update front + fix header

--- src/controllers/admin-controller.h
+++ src/controllers/admin-controller.h
@@ -6,6 +6,8 @@
 char *ctr_create_table(char* table_name, char* json_columns);
 char *ctr_delete_table(char* table_name);
 
+char *ctr_get_admins();
+
 // LOGS
 char *ctr_get_logs();
 
@@ -14,7 +16,6 @@ char *ctr_get_configs();
 char *ctr_set_config_value(char *config_name, char* config_value);
 
 char *ctr_get_config_value_str(char *config_name);
-bool ctr_get_config_value_boolean(char *config_name);
 int ctr_get_config_value_int(char *config_name);
 double ctr_get_config_value_double(char *config_name);
 
